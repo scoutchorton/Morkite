10ccec4015b2368a54b8034193a90c03d9d3b2a4
Caleb Horton <scoutchorton@scoutchorton.io>
2022-12-26 23:18:52 -0500

    Integrated Lua

--- src/test.cpp
+++ src/test.cpp
@@ -2,6 +2,8 @@
 
 #include <SDL2/SDL.h>
 #include <lua5.4/lua.h>
+#include <lua5.4/lualib.h>
+#include <lua5.4/lauxlib.h>
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
@@ -11,6 +13,12 @@ Resources
 
 SDL Tutorials: https://lazyfoo.net/tutorials/SDL/index.php
 */
+
+// Globals
+/** @todo remove later */
+SDL_Window* window = NULL;
+
+int setWindowTitle_L(lua_State*);
 
 SDL_Window* initSDL() {
 	SDL_Window* window = NULL;
@@ -36,17 +44,48 @@ void quitSDL(SDL_Window* window) {
 	SDL_Quit();
 }
 
+lua_State* initLua() {
+	lua_State* Lua = luaL_newstate();
+	luaL_openlibs(Lua);
+
+	lua_pushcfunction(Lua, setWindowTitle_L);
+	lua_setglobal(Lua, "setWindowTitle");
+
+	return Lua;
+}
+
+int setWindowTitle_L(lua_State* Lua) {
+	const char* windowNameArg = NULL;
+	//std::string windowName;
+
+	// Get the new name and convert to C++ string
+	windowNameArg = lua_tolstring(Lua, 1, NULL);
+	//windowName = windowNameArg;
+	SDL_SetWindowTitle(window, windowNameArg);
+
+	// Lua return thing
+	//lua_pushnil(Lua);
+
+	return 0;
+}
+
 int main(int argc, char** argv) {
+	lua_State* Lua;
 	SDL_Surface* surface = NULL;
-	SDL_Window* window = NULL;
 	bool quit = false;
 	Uint8 red = 0;
 	SDL_Event e;
 
+	int hasRunLua = 0;
+
+	// Initialization
 	window = initSDL();
+	Lua = initLua();
 
 	// Handle test drawing
 	surface = SDL_GetWindowSurface(window);
+
+	luaL_dofile(Lua, "./src/test.lua");
 
 	// Wait for quit command from user
 	while(quit == false) {
@@ -57,8 +96,11 @@ int main(int argc, char** argv) {
 		}
 
 		red++;
+		hasRunLua++;
 
 		// Game logic
+		if(hasRunLua == 100000)
+			luaL_dofile(Lua, "./src/test.lua");
 
 		// Drawing logic
 		SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, red, 0, 0));
---
+++ src/test.lua
@@ -0,0 +1,9 @@
+local x = 0
+
+while x < 10 do
+	print('Hello world from Lua! #'..x)
+	x = x + 1
+	--sleep(10)
+end
+
+setWindowTitle('PROJECT MORKITE: NOW WITH LUA!!!')
